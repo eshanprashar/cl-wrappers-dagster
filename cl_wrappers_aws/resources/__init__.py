d151c12096438e67c307c999a44c32d5b36031d2
eshanprashar <eshanprashar@uchicago.edu>
2024-09-25 11:19:14 -0500

    added conda env, modified base class

--- cl_wrappers_aws/resources/__init__.py
+++ cl_wrappers_aws/resources/__init__.py
@@ -1,3 +1,6 @@
 from dagster import EnvVar
 
-api_token = EnvVar("API_RESOURCE")
+
+# #EnvVar defers resolution of the environment variable value until run time, 
+# and should only be used as input to Dagster config or resources.
+api_token = EnvVar("API_TOKEN")
