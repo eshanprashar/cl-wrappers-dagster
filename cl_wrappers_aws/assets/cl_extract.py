a726c8248cf4f639798f18e4cfa0fca0caf99579
eshanprashar <eshanprashar@uchicago.edu>
2024-09-26 00:33:35 -0500

    added asset to clean positions df

--- cl_wrappers_aws/assets/cl_extract.py
+++ cl_wrappers_aws/assets/cl_extract.py
@@ -6,13 +6,16 @@ from cl_wrappers_aws.resources.api_scraper import APIScraper, CLScraper
 api_token_var = EnvVar("API_TOKEN")
 
 @asset
-def position_csv_files(context:OpExecutionContext, s3: S3Resource) -> None:
+def position_csv_files(context:OpExecutionContext, s3: S3Resource) -> dict:
     # Retrieve the api token
     api_token = api_token_var.get_value()
 
     # Create the CLScraper object
     cl_scraper = CLScraper(api_token=api_token,context=context)
     
+    # Define storage type 
+    storage_type = 'local'
+
     # Log start of the process
     context.log.info("Starting position data extraction...")
     cl_scraper.fetch_positions(
@@ -21,12 +24,17 @@ def position_csv_files(context:OpExecutionContext, s3: S3Resource) -> None:
         save_logic= 'save_after_pages', # Possible options are 'author_level' or 'save_after_pages'
         num_pages_to_save = 5,
         max_pages=20,
-        storage_type='local',  # or 'local' depending on your setup
+        storage_type=storage_type,  # or 'local' depending on your setup
         s3_bucket=None,
         s3_key=None,
         # Add any additional parameters if needed
     )
     context.log.info("Position data extraction completed.")
+    # Return metadata: temporary fix to handle dependency in clean_positions_dataframe
+    return {
+        "storage_type": storage_type
+    }
+
 
 @asset
 def financial_disclosures_csv_files(context:OpExecutionContext, s3: S3Resource) -> None:
